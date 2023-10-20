bca828e060157b23633305a2807e2788e6f7a371
AHTepsilon <alehertoro725@gmail.com>
2023-10-20 12:52:36 -0500

    commited files

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD95uiNWmLwQAzLR8Ms0TCfGJZ1cy9JKdk",
+  authDomain: "esp32-cam-project-1.firebaseapp.com",
+  projectId: "esp32-cam-project-1",
+  storageBucket: "esp32-cam-project-1.appspot.com",
+  messagingSenderId: "645341830458",
+  appId: "1:645341830458:web:fbc737e945dbd56ccd8403"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
